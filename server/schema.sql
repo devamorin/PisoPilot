02e0d1b6714b698e258c9065b8fa2f0f95d6f798
CAmorin28 <christianpaulamorin28@gmail.com>
2026-06-25 16:50:48 +0800

    [fix][Amorin] Deployment fix 3

--- server/schema.sql
+++ server/schema.sql
@@ -1,8 +1,8 @@
 -- PisoPilot Database Schema
 
 -- Create database
-CREATE DATABASE IF NOT EXISTS pisopilot;
-USE pisopilot;
+CREATE DATABASE IF NOT EXISTS railway;
+USE railway;
 
 -- Users table
 CREATE TABLE IF NOT EXISTS users (
